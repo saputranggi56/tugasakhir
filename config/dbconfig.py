719007dac0813c5a388e9534cd5c8f877fdff031
brianlyhr <56231364+saputranggi56@users.noreply.github.com>
2022-12-24 01:26:28 +0700

    connection

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -2,9 +2,9 @@
 import psycopg2
 import psycopg2.extras
 
-DB_HOST = "localhost"
-DB_NAME = "skripsi"
+DB_HOST = "103.126.28.66"
+DB_NAME = "news"
 DB_USER = "postgres"
-DB_PASS = ""
+DB_PASS = "khansia215758"
 
 CONN    = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
